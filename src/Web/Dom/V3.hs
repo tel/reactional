1447a99936a5f9545428c7ce4041a2e23fbdc978
Joseph Abrahamson <joseph@reifyhealth.com>
2014-06-29 18:47:30 -0400

    Some operational functions

--- src/Web/Dom/V3.hs
+++ src/Web/Dom/V3.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE RecursiveDo                #-}
 {-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TypeFamilies               #-}
 
-module Web.Dom.V3 where
+module Web.Dom.V3 (
+  Virt
+  ) where
 
 import           Control.Applicative
 import           Control.Monad.ST
 import           Control.Monad.State.Strict
 import           Data.Monoid
-import           Data.Sequence              (Seq, (<|), (|>), ViewL (..), ViewR (..))
+import           Data.Sequence              (Seq, ViewL (..), ViewR (..), (<|),
+                                             (|>))
 import qualified Data.Sequence              as Seq
 import           Data.STRef
 import qualified Data.Traversable           as T
@@ -27,6 +32,9 @@ readHere = liftST . readSTRef . here
 
 liftST :: ST s a -> Virt s a
 liftST = Virt . lift
+
+liftSt :: StateT [ANode (Virt s)] (ST s) a -> Virt s a
+liftSt = Virt
 
 newNode :: St.NodeF ty (ANode (Virt s)) -> ST s (Node (Virt s) ty)
 newNode e = N <$> newSTRef e <*> newSTRef Nothing
@@ -137,14 +145,25 @@ instance DomNode (Virt s) where
       St.El (St.Element theTag attr) cs <- readHere n
       found <- T.mapM lookupTag' cs
       let found' = join found
-      if tg == theTag 
+      if tg == theTag
         then return (n <| found')
         else return found'
-      
+
     lookupTag' :: ANode (Virt s) -> Virt s (Seq (Node (Virt s) 'El))
     lookupTag' x = case x of
       Left e  -> lookupTag e
       Right t -> return mempty
+
+  getElementsByTagName Nothing tg = do
+    els <- liftSt get
+    all <- mapM getRecursive els
+    return (join (Seq.fromList all))
+    where
+      getRecursive :: ANode (Virt s) -> Virt s (Seq (Node (Virt s) 'El))
+      getRecursive x = case x of
+        Left e  -> getElementsByTagName (Just e) tg
+        Right x -> return mempty
+
 
 nodeEq :: (INodeType ty, INodeType ty')
         => Node (Virt s) ty -> Node (Virt s) ty' -> Bool
@@ -158,3 +177,54 @@ smashl at seq =
     a :< s -> (l, Just a, s)
 
 deriving instance Eq (Node (Virt s) ty)
+
+
+--------------------------------------------------------------------------------
+-- Running and manipulating Virtual DOMs
+
+-- | Converts a static dom node into a reference to a virtual one.
+explode :: St.Node -> ST s (ANode (Virt s))
+explode = explodeP Nothing where
+  explodeP :: Maybe (Node (Virt s) 'El) -> St.Node -> ST s (ANode (Virt s))
+  explodeP m (St.Node x) = do
+    case x of
+      St.El e cs -> mdo
+        cs'  <- T.mapM (explodeP (Just me)) cs
+        me   <- N <$> newSTRef (St.El e cs')
+                  <*> newSTRef m
+        return (Left me)
+      St.Tx t -> do
+        me <- N <$> newSTRef (St.Tx t)
+                <*> newSTRef m
+        return (Right me)
+
+-- | Assume the existence of a particular 'St.Node' in the context of
+-- a virtual dom.
+assume :: St.Node -> Virt s ()
+assume n = do
+  s <- liftST (explode n)
+  liftSt (modify (s :))
+
+-- | Run a virtual dom operation assuming the existence of some nodes.
+runVirt' :: (forall s . Virt s a) -> [St.Node] -> a
+runVirt' v xs = runST $ do
+  refs <- mapM explode xs
+  evalStateT (unVirt v) refs
+
+-- | Run a virtual dom operation in an empty environment.
+runVirt :: (forall s . Virt s a) -> a
+runVirt v = runVirt' v []
+
+-- | Compute the static form of a virtual reference
+reify :: Node (Virt s) ty -> Virt s (St.NodeF ty St.Node)
+reify = T.traverse reifyAny <=< readHere
+
+-- | Compute the static form of a virtual reference without knowing
+-- what kind will result.
+reifyAny :: ANode (Virt s) -> Virt s St.Node
+reifyAny x = case x of
+  Left  e -> St.Node <$> reify e
+  Right t -> St.Node <$> reify t
+
+reifyAll :: Virt s [St.Node]
+reifyAll = liftSt get >>= mapM reifyAny
