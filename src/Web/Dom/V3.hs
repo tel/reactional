956e81f91525832b68302ea7e6fbec65a3e75ab5
Joseph Abrahamson <joseph@reifyhealth.com>
2014-06-29 16:19:58 -0400

    A few more DomNode functions implemented

--- src/Web/Dom/V3.hs
+++ src/Web/Dom/V3.hs
@@ -10,6 +10,8 @@ import           Control.Applicative
 import           Control.Monad.ST
 import           Control.Monad.State.Strict
 import           Data.Monoid
+import           Data.Sequence              (Seq, (<|), (|>), ViewL (..), ViewR (..))
+import qualified Data.Sequence              as Seq
 import           Data.STRef
 import qualified Data.Traversable           as T
 import           Web.Dom
@@ -28,6 +30,14 @@ liftST = Virt . lift
 
 newNode :: St.NodeF ty (ANode (Virt s)) -> ST s (Node (Virt s) ty)
 newNode e = N <$> newSTRef e <*> newSTRef Nothing
+
+modE :: (St.Element -> Seq (ANode (Virt s)) -> (St.Element, Seq (ANode (Virt s))))
+     -> Node (Virt s) El -> Virt s ()
+modE f n = do
+  x  <- readHere n
+  let x' = case x of
+        St.El e cs -> uncurry St.El (f e cs)
+  liftST (writeSTRef (here n) x')
 
 instance DomNode (Virt s) where
   data Node (Virt s) ty =
@@ -66,5 +76,70 @@ instance DomNode (Virt s) where
       Right n -> Right <$> go n
 
   isEqualNode n1 n2 = return (n1 == n2)
+
+  -- always removes child first
+  appendChild p c = do
+    ok <- case forgetNode c of
+      Left ce -> contains ce p
+      Right t -> return True
+    case ok of
+      False -> return (Left hierarchyErrorMessage)
+      True  -> do
+        x <- removeChild p c
+        case x of
+          Left err -> return x
+          Right ()  -> do
+            liftST (writeSTRef (parent c) (Just p))
+            modE (\e cs -> (e, cs |> forgetNode c)) p
+            return (Right ())
+    where
+     hierarchyErrorMessage = "HierarchyRequestError: Failed to execute 'appendChild' on 'Node': The new child element contains the parent."
+
+  insertBefore p r c = do
+    ok <- case forgetNode c of
+      Left ce -> contains ce p
+      Right t -> return True
+    case ok of
+      False -> return (Left hierarchyErrorMessage)
+      True  -> do
+        x <- removeChild p c
+        case x of
+          Left err -> return x
+          Right ()  -> do
+            liftST (writeSTRef (parent c) (Just p))
+            modE (\e cs -> (e, insertBeforeS cs (forgetNode r) (forgetNode c))) p
+            return (Right ())
+    where
+     hierarchyErrorMessage = "HierarchyRequestError: Failed to execute 'appendChild' on 'Node': The new child element contains the parent."
+     insertBeforeS :: Seq (ANode (Virt s))
+                   -> ANode (Virt s)
+                   -> ANode (Virt s)
+                   -> Seq (ANode (Virt s))
+     insertBeforeS s r i = case smashl (== r) s of
+       (l, Nothing, _) -> l |> i
+       (l, Just h,  r) -> l <> (i <| h <| r)
+
+  removeChild p c = do
+    x <- contains p c
+    case x of
+      False -> return (Left notFoundErrorMessage)
+      True -> do
+        modE (\e cs -> (e, Seq.filter (anyNode (not . nodeEq c)) cs)) p
+        liftST (writeSTRef (parent c) Nothing)
+        return (Right ())
+    where
+      notFoundErrorMessage =
+        "NotFoundError: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node."
+
+nodeEq :: (INodeType ty, INodeType ty')
+        => Node (Virt s) ty -> Node (Virt s) ty' -> Bool
+nodeEq n1 n2 = forgetNode n1 == forgetNode n2
+
+smashl :: (a -> Bool) -> Seq a -> (Seq a, Maybe a, Seq a)
+smashl at seq =
+  let (l, r) = Seq.breakl at seq
+  in case Seq.viewl r of
+    EmptyL -> (l, Nothing, r)
+    a :< s -> (l, Just a, s)
 
 deriving instance Eq (Node (Virt s) ty)
